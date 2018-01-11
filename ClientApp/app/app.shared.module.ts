66b98ef15bc5801b74da36601b6d62019484e444
Bruno <brunocoelho@conveniar.com.br>
2018-01-11 15:19:59 -0200

    nao ta rolando

--- ClientApp/app/app.shared.module.ts
+++ ClientApp/app/app.shared.module.ts
@@ -5,7 +5,6 @@ import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './components/app/app.component';
-import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
---
+++ Models/Analise.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace asp_angular.Models
+{
+    public class Analise
+    {
+        public int id;
+        public string nome;
+        public int clienteId;
+        public string descricao;
+        public DateTime dataFim;
+        public double valorProposta;
+    }
+}
